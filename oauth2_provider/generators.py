d0e98d72b87b36efbfc185e28ad6db93c0e31540
Federico Frenguelli <synasius@gmail.com>
2013-05-22 18:11:23 +0200

    Now it's possible to override client secret and client generator classes

--- oauth2_provider/generators.py
+++ oauth2_provider/generators.py
@@ -1,3 +1,4 @@
+from .settings import oauth2_settings
 from .utils import long_hash, short_hash
 
 
@@ -6,7 +7,7 @@ class BaseHashGenerator(object):
     All generators should extend this class overriding `.hash()` method.
     """
     def hash(self):
-        raise NotImplementedError
+        raise NotImplementedError()
 
 
 class ClientIdGenerator(BaseHashGenerator):
@@ -23,13 +24,13 @@ def generate_client_id():
     """
     Generate a suitable client id
     """
-    client_id_generator = ""
-    return ClientIdGenerator().hash()
+    client_id_generator = oauth2_settings.CLIENT_ID_GENERATOR_CLASS()
+    return client_id_generator.hash()
 
 
 def generate_client_secret():
     """
     Generate a suitable client secret
     """
-    client_secret_generator = ""
-    return ClientSecretGenerator().hash()
+    client_secret_generator = oauth2_settings.CLIENT_SECRET_GENERATOR_CLASS()
+    return client_secret_generator.hash()
---
+++ oauth2_provider/settings.py
@@ -0,0 +1,114 @@
+"""
+This module is largely inspired by django-rest-framework settings.
+
+Settings for the OAuth2 Provider are all namespaced in the OAUTH2_PROVIDER setting.
+For example your project's `settings.py` file might look like this:
+
+OAUTH2_PROVIDER = {
+    'CLIENT_ID_GENERATOR_CLASS':
+        'oauth2_provide.generators.ClientIdGenerator',
+    'CLIENT_SECRET_GENERATOR_CLASS':
+        'oauth2_provide.generators.ClientSecretGenerator',
+}
+
+This module provides the `oauth2_settings` object, that is used to access
+OAuth2 Provider settings, checking for user settings first, then falling
+back to the defaults.
+"""
+from __future__ import unicode_literals
+
+import six
+
+from django.conf import settings
+from django.utils import importlib
+
+
+USER_SETTINGS = getattr(settings, 'OAUTH2_PROVIDER', None)
+
+DEFAULTS = {
+    'CLIENT_ID_GENERATOR_CLASS':
+        'oauth2_provider.generators.ClientIdGenerator',
+    'CLIENT_SECRET_GENERATOR_CLASS':
+        'oauth2_provider.generators.ClientSecretGenerator',
+}
+
+# List of settings that cannot be empty
+MANDATORY = (
+    'CLIENT_ID_GENERATOR_CLASS',
+    'CLIENT_SECRET_GENERATOR_CLASS',
+)
+
+# List of settings that may be in string import notation.
+IMPORT_STRINGS = (
+    'CLIENT_ID_GENERATOR_CLASS',
+    'CLIENT_SECRET_GENERATOR_CLASS',
+)
+
+
+def perform_import(val, setting_name):
+    """
+    If the given setting is a string import notation,
+    then perform the necessary import or imports.
+    """
+    if isinstance(val, six.string_types):
+        return import_from_string(val, setting_name)
+    elif isinstance(val, (list, tuple)):
+        return [import_from_string(item, setting_name) for item in val]
+    return val
+
+
+def import_from_string(val, setting_name):
+    """
+    Attempt to import a class from a string representation.
+    """
+    try:
+        parts = val.split('.')
+        module_path, class_name = '.'.join(parts[:-1]), parts[-1]
+        module = importlib.import_module(module_path)
+        return getattr(module, class_name)
+    except ImportError as e:
+        msg = "Could not import '%s' for setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
+        raise ImportError(msg)
+
+
+class OAuth2ProviderSettings(object):
+    """
+    A settings object, that allows OAuth2 Provider settings to be accessed as properties.
+
+    Any setting with string import paths will be automatically resolved
+    and return the class, rather than the string literal.
+    """
+
+    def __init__(self, user_settings=None, defaults=None, import_strings=None, mandatory=None):
+        self.user_settings = user_settings or {}
+        self.defaults = defaults or {}
+        self.import_strings = import_strings or ()
+        self.mandatory = mandatory or ()
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults.keys():
+            raise AttributeError("Invalid OAuth2Provider setting: '%s'" % attr)
+
+        try:
+            # Check if present in user settings
+            val = self.user_settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            val = self.defaults[attr]
+
+        # Coerce import strings into classes
+        if val and attr in self.import_strings:
+            val = perform_import(val, attr)
+
+        self.validate_setting(attr, val)
+
+        # Cache the result
+        setattr(self, attr, val)
+        return val
+
+    def validate_setting(self, attr, val):
+        if not val and attr in self.mandatory:
+            raise AttributeError("OAuth2Provider setting: '%s' is mandatory" % attr)
+
+
+oauth2_settings = OAuth2ProviderSettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS, MANDATORY)
