9b7f05fb6ad2e523fdf22f0f1bc35a542f8a638d
Massimiliano <masci@evonove.it>
2013-05-24 14:55:38 +0200

    added fields to the authorization form

--- oauth2_provider/forms.py
+++ oauth2_provider/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class AllowForm(forms.Form):
     allow = forms.BooleanField()
+    redirect_uri = forms.URLField(widget=forms.HiddenInput())
+    scopes = forms.CharField(widget=forms.HiddenInput())
+    client_id = forms.CharField(widget=forms.HiddenInput())
+    state = forms.CharField(widget=forms.HiddenInput())
