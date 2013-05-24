e8bbfc8cee78e0ff2f8c56c3537f981faafb2d15
Federico Frenguelli <synasius@gmail.com>
2013-05-24 17:51:10 +0200

    use exmpla project settings for test

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+    example_project_dir = os.path.join(os.path.dirname(__file__), 'example')
+    sys.path.insert(0, example_project_dir)
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
