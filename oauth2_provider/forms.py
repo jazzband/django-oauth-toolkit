dfa39db42cc5ce2c29da2ec0c388865ec7f41030
Federico Frenguelli <synasius@gmail.com>
2013-05-28 16:09:39 +0200

    Add allow field to form

--- oauth2_provider/forms.py
+++ oauth2_provider/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class AllowForm(forms.Form):
+    allow = forms.BooleanField(required=False)
     redirect_uri = forms.URLField(widget=forms.HiddenInput())
     scopes = forms.CharField(required=False, widget=forms.HiddenInput())
     client_id = forms.CharField(widget=forms.HiddenInput())
