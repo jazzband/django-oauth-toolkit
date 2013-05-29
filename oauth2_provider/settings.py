e1f1f2ed1d17b91e3dcebda313b77c05bfef4b5f
Massimiliano <masci@evonove.it>
2013-05-29 14:18:47 +0200

    specify SCOPES in settings file

--- oauth2_provider/settings.py
+++ oauth2_provider/settings.py
@@ -28,6 +28,7 @@ USER_SETTINGS = getattr(settings, 'OAUTH2_PROVIDER', None)
 DEFAULTS = {
     'CLIENT_ID_GENERATOR_CLASS': 'oauth2_provider.generators.ClientIdGenerator',
     'CLIENT_SECRET_GENERATOR_CLASS': 'oauth2_provider.generators.ClientSecretGenerator',
+    'SCOPES': [],
 }
 
 # List of settings that cannot be empty
