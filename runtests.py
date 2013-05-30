2eca1419d9220c008991b03cb7765d0984f9ef50
Federico Frenguelli <synasius@gmail.com>
2013-05-30 15:39:56 +0200

    Add a runtests module

---
+++ runtests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--coverage", dest="coverage", action='store_true')
+
+    args = parser.parse_args()
+    if args.coverage:
+        test_cmd = "test_coverage"
+    else:
+        test_cmd = "test"
+
+    app_to_test = "oauth2_provider"
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+    example_project_dir = os.path.join(os.path.dirname(__file__), 'example')
+    sys.path.insert(0, example_project_dir)
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([sys.argv[0], test_cmd, app_to_test])
