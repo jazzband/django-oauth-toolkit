8c62e03bcf080ca814a53562cf80913d117fc2a5
Federico Frenguelli <synasius@gmail.com>
2013-05-31 12:17:45 +0200

    Add ScopedResourceMixin tests

--- oauth2_provider/tests/test_mixins.py
+++ oauth2_provider/tests/test_mixins.py
@@ -4,7 +4,7 @@ from django.test import TestCase, RequestFactory
 
 from oauthlib.oauth2 import Server
 
-from ..mixins import OAuthLibMixin
+from ..mixins import OAuthLibMixin, ScopedResourceMixin
 from ..oauth2_validators import OAuth2Validator
 
 
@@ -41,3 +41,27 @@ class TestOAuthLibMixin(TestCase):
         test_view = TestView()
 
         self.assertIsInstance(test_view.get_server(request), Server)
+
+
+class TestScopedResourceMixin(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.request_factory = RequestFactory()
+
+    def test_missing_requested_scopes(self):
+        class TestView(ScopedResourceMixin, View):
+            pass
+
+        request = self.request_factory.get("/fake-req")
+        test_view = TestView()
+
+        self.assertRaises(ImproperlyConfigured, test_view.get_scopes)
+
+    def test_correct_requested_scopes(self):
+        class TestView(ScopedResourceMixin, View):
+            requested_scopes = ['scope1', 'scope2']
+
+        request = self.request_factory.get("/fake-req")
+        test_view = TestView()
+
+        self.assertEqual(test_view.get_scopes(), ['scope1', 'scope2'])
