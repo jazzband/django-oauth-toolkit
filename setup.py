da38008487f91a8e9a0eccec6bdcad1b388f27c1
Federico Frenguelli <synasius@gmail.com>
2013-05-31 13:04:11 +0200

    Add package data to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@
 
 from setuptools import setup
 import os
+import re
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 def get_packages(package):
@@ -14,11 +23,30 @@ def get_packages(package):
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('oauth2_provider')
+
+
 LONG_DESCRIPTION = open('README.rst').read()
 
 setup(
     name="django-oauth-toolkit",
-    version="0.1.0",
+    version=version,
     description="OAuth2 goodies for the Djangonauts",
     long_description=LONG_DESCRIPTION,
     classifiers=[
@@ -36,7 +64,7 @@ setup(
     url='http://github.com/pydanny/django-admin2',
     license='BSD',
     packages=get_packages('oauth2_provider'),
-    include_package_data=True,
+    package_data=get_package_data('oauth2_provider'),
     test_suite='runtests',
     install_requires=[
         'django>=1.5.0',
