ccb3b554f9c787b5b2ba80d7afe1833b2c4da339
Federico Frenguelli <synasius@gmail.com>
2013-06-03 16:55:00 +0200

    Add helper function for django 1.4 compatibility

--- oauth2_provider/compat.py
+++ oauth2_provider/compat.py
@@ -8,3 +8,18 @@ try:
     from urllib import urlencode
 except ImportError:
     from urllib.parse import urlencode
+
+# Django 1.5 add support for custom auth user model
+import django
+if django.VERSION >= (1, 5):
+    from django.conf import settings
+    if hasattr(settings, 'AUTH_USER_MODEL'):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+    else:
+        from django.contrib.auth.models import User
+else:
+    try:
+        from django.contrib.auth.models import User
+    except ImportError:
+        raise ImportError("User model is not to be found.")
