7fb5dae7c032d98d6bc25eab54dd0ea99e94a5a3
Massimiliano <masci@evonove.it>
2013-06-04 12:25:13 +0200

    added middleware to allow CORS

--- example/example/settings.py
+++ example/example/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'example.middleware.XsSharingMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
