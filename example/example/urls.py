8f6f569c73681da98da824ae572d5bc990ce2b82
Massimiliano <masci@evonove.it>
2013-06-04 15:27:49 +0200

    move exchange endpoint into consumer urlspace

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
-from .views import ConsumerView, ConsumerExchangeView
+from .views import ConsumerView,    ConsumerExchangeView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='example/home.html'), name='home'),
-    url(r'^exchange/', ConsumerExchangeView.as_view(), name='exchange'),
+    url(r'^consumer/exchange/', ConsumerExchangeView.as_view(), name='exchange'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'example/login.html'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^o/', include('oauth2_provider.urls')),
