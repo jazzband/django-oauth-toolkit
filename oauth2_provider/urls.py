bebccdd2919389675cb513c2ded7d96d993a78e2
Federico Frenguelli <synasius@gmail.com>
2013-07-08 12:22:27 +0200

    application management urls grouped

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -7,7 +7,12 @@ urlpatterns = patterns(
     '',
     url(r'^authorize/$', views.AuthorizationView.as_view(), name="authorize"),
     url(r'^token/$', views.TokenView.as_view(), name="token"),
-    url(r'^registration/$', views.RegistrationView.as_view(), name="registration"),
+)
+
+# Application management views
+urlpatterns += patterns(
+    '',
     url(r'^applications/$', views.ApplicationList.as_view(), name="applications_list"),
-    url(r'^applications/(?P<pk>\d)$', views.ApplicationDetail.as_view(), name="applications_detail"),
+    url(r'^applications/register/$', views.RegistrationView.as_view(), name="applications_register"),
+    url(r'^applications/(?P<pk>\d+)$', views.ApplicationDetail.as_view(), name="applications_detail"),
 )
