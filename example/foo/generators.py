3caa7503be505d9d9463e7ab7354f4f87fb7a3ce
Massimiliano <masci@evonove.it>
2013-07-20 17:06:30 +0200

    removed foo application

--- example/foo/generators.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-from oauth2_provider.generators import BaseHashGenerator
-
-
-class ClientIdGenerator(BaseHashGenerator):
-    def hash(self):
-        return ''.join(random.choice(string.letters + string.digits) for i in range(100))
