b26a1dc9ece162c7cd6c9c8f9157db6ce10b1a24
Massimiliano Pippi <masci@evonove.it>
2013-08-30 00:16:40 +0200

    more api methods

--- example/example/api_v1.py
+++ example/example/api_v1.py
@@ -4,10 +4,10 @@ from oauth2_provider import VERSION
 import json
 from django.http import HttpResponse
 from django import get_version
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponseBadRequest, HttpResponseNotFound
 
 from oauthlib.oauth2 import Server
 
@@ -20,7 +20,8 @@ class MyServer(Server):
     """
     def verify_request(self, uri, http_method='GET', body=None, headers=None, scopes=None):
         ok, request = super(MyServer, self).verify_request(uri, http_method, body, headers, scopes)
-        ok = request.http_method == 'GET'
+        if request.http_method == 'GET':
+            ok = True  # possibly override failures
         return ok, request
 
 
@@ -38,6 +39,7 @@ def get_system_info(request, *args, **kwargs):
     return HttpResponse(json.dumps(data), content_type='application/json', *args, **kwargs)
 
 
+@csrf_exempt
 @protected_resource(server_cls=MyServer)
 @require_http_methods(["GET", "POST"])
 def applications_list(request, *args, **kwargs):
@@ -48,20 +50,44 @@ def applications_list(request, *args, **kwargs):
     if request.method == 'GET':
         data = serializers.serialize("json", MyApplication.objects.all())
         return HttpResponse(data, content_type='application/json', *args, **kwargs)
+    elif request.method == 'POST':
+        if request.is_ajax():
+            try:
+                data = json.loads(request.body)
+                data['user'] = request.resource_owner
+                obj = MyApplication.objects.create(**data)
+                out = serializers.serialize("json", [obj])
+            except (ValueError, TypeError):
+                return HttpResponseBadRequest()
+
+            return HttpResponse(out, content_type='application/json', status=201, *args, **kwargs)
 
 
+@csrf_exempt
 @protected_resource()
 @require_http_methods(["GET", "PUT", "DELETE"])
 def applications_detail(request, pk, *args, **kwargs):
     """
     Show resource with GET, update it with PUT, destroy with DELETE
     """
-    qs = MyApplication.objects.filter(user=request.resource_owner).filter(pk=pk)
+    try:
+        resource = MyApplication.objects.filter(user=request.resource_owner).filter(pk=pk).get()
+    except MyApplication.DoesNotExist:
+        return HttpResponseNotFound()
+
     if request.method == 'GET':
-        if len(qs):
-            data = serializers.serialize("json", qs)
-            status = 200
-        else:
-            data = json.dumps({'message': 'Object not found'})
-            status = 404
-        return HttpResponse(data, content_type='application/json', status=status, *args, **kwargs)
+        data = serializers.serialize("json", [resource])
+        return HttpResponse(data, content_type='application/json', status=200, *args, **kwargs)
+    elif request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+            for k, v in data.iteritems():
+                setattr(resource, k, v)
+            resource.save()
+            data = serializers.serialize("json", [resource])
+            return HttpResponse(data, content_type='application/json')
+        except (ValueError, TypeError):
+            return HttpResponseBadRequest()
+    elif request.method == 'DELETE':
+        resource.delete()
+        return HttpResponse(status=204)
