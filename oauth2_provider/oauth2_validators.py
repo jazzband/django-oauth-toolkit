0e71410d550649e3b23dc1e68ab4a87722038848
Luca Corti <luca.corti@sparkling18.com>
2013-09-17 14:50:27 +0200

    Revert previous

--- oauth2_provider/oauth2_validators.py
+++ oauth2_provider/oauth2_validators.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 import base64
-import urlib
 import logging
 from datetime import timedelta
 
@@ -45,7 +44,7 @@ class OAuth2Validator(RequestValidator):
         client_id, client_secret = auth_string_decoded.split(':', 1)
 
         try:
-            request.client = Application.objects.get(client_id=urllib.unquote_plus(client_id), client_secret=urllib.unquote_plus(client_secret))
+            request.client = Application.objects.get(client_id=client_id, client_secret=client_secret)
             return True
 
         except Application.DoesNotExist:
