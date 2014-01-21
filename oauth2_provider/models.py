f1760d3a7c8e500d017e44c8943f803c0a2f0c12
Warnar Boekkooi <warnar@boekkooi.net>
2014-01-21 10:57:31 +0800

    Use ugettext_lazy to avoid django dev branch error

--- oauth2_provider/models.py
+++ oauth2_provider/models.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 from django.db.models import get_model
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.core.exceptions import ImproperlyConfigured
 
