d612e54e3c31527431b8fc707e986857a68085fb
Jason Filipe <jason.filipe@gmail.com>
2014-01-28 12:47:39 -0500

    Added support for singular `scope` query parameter.

--- oauth2_provider/forms.py
+++ oauth2_provider/forms.py
@@ -6,10 +6,17 @@ from .models import get_application_model
 class AllowForm(forms.Form):
     allow = forms.BooleanField(required=False)
     redirect_uri = forms.CharField(widget=forms.HiddenInput())
-    scopes = forms.CharField(required=False, widget=forms.HiddenInput())
+    scope = forms.CharField(required=False, widget=forms.HiddenInput())
     client_id = forms.CharField(widget=forms.HiddenInput())
     state = forms.CharField(required=False, widget=forms.HiddenInput())
     response_type = forms.CharField(widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        data = kwargs.get('data')
+        # backwards compatible support for plural `scopes` query parameter
+        if data and 'scopes' in data:
+            data['scope'] = data['scopes']
+        return super(AllowForm, self).__init__(*args, **kwargs)
 
 
 class RegistrationForm(forms.ModelForm):
