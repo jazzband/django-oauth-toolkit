ce6f1705ee1b201382c4de9755861eadabd8c7bc
Axel Haustant <noirbizarre@users.noreply.github.com>
2014-02-18 16:50:58 +0100

    Don't pin django-braces and six versions
    These two pinned dependencies make the package uncompatible with other (or at least some of my corporate project)
    
    I just accepted latest versions.

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     test_suite='runtests',
     install_requires=[
         'django>=1.4',
-        'django-braces==1.2.2',
-        'six==1.3.0',
+        'django-braces>=1.2.2',
+        'six>=1.3.0',
         'oauthlib==0.6.1',
     ],
     zip_safe=False,
