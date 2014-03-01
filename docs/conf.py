592bd7198d16da38f76b5954110c9831af8672d8
masci <mpippi@gmail.com>
2014-03-01 16:28:58 +0100

    minor adjustments and warnings suppression

--- docs/conf.py
+++ docs/conf.py
@@ -13,14 +13,15 @@
 
 import sys, os, re
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings'
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../example'))
+here = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, here)
+sys.path.insert(0, os.path.join(here, '..'))
+sys.path.insert(0, os.path.join(here, '..', 'example'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings.dev'
 import oauth2_provider
 
 # -- General configuration -----------------------------------------------------
@@ -137,7 +138,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
