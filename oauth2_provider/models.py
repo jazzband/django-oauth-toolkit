f5da293e86aa4ca2d5e0f22e337314e510d56c89
Thomas Chaumeny <thomas.chaumeny@polyconseil.fr>
2014-09-01 17:04:19 +0200

    Add an index on AccessToken.token to speedup token verification

--- oauth2_provider/models.py
+++ oauth2_provider/models.py
@@ -165,7 +165,7 @@ class AccessToken(models.Model):
     * :attr:`scope` Allowed scopes
     """
     user = models.ForeignKey(AUTH_USER_MODEL)
-    token = models.CharField(max_length=255)
+    token = models.CharField(max_length=255, db_index=True)
     application = models.ForeignKey(oauth2_settings.APPLICATION_MODEL)
     expires = models.DateTimeField()
     scope = models.TextField(blank=True)
