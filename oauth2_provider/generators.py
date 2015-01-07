42293001eb0bf54bc0f8d9bc83a4728f146974bc
Massimiliano Pippi <mpippi@gmail.com>
2015-01-07 17:43:44 +0100

    changed default character set for client_id and client_secret generators, fixes #167 #170

--- oauth2_provider/generators.py
+++ oauth2_provider/generators.py
@@ -1,9 +1,10 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from oauthlib.common import generate_client_id as oauthlib_generate_client_id
+from oauthlib.common import UNICODE_ASCII_CHARACTER_SET
 
 from .settings import oauth2_settings
-
-
-CLIENT_ID_CHARACTER_SET = r'_-.:;=?!@0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 
 class BaseHashGenerator(object):
@@ -20,13 +21,13 @@ class ClientIdGenerator(BaseHashGenerator):
         Generate a client_id without colon char as in http://tools.ietf.org/html/rfc2617#section-2
         for Basic Authentication scheme
         """
-        client_id_charset = CLIENT_ID_CHARACTER_SET.replace(":", "")
-        return oauthlib_generate_client_id(length=40, chars=client_id_charset)
+        return oauthlib_generate_client_id(length=40, chars=UNICODE_ASCII_CHARACTER_SET)
 
 
 class ClientSecretGenerator(BaseHashGenerator):
     def hash(self):
-        return oauthlib_generate_client_id(length=oauth2_settings.CLIENT_SECRET_GENERATOR_LENGTH, chars=CLIENT_ID_CHARACTER_SET)
+        return oauthlib_generate_client_id(length=oauth2_settings.CLIENT_SECRET_GENERATOR_LENGTH,
+                                           chars=UNICODE_ASCII_CHARACTER_SET)
 
 
 def generate_client_id():
