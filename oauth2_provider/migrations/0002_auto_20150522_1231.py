264d3077a64eff51ba6304a64be5b275e6cb3efa
Dirk Moors <dirkmoors@gmail.com>
2015-05-22 14:43:27 +0200

    Fixed bug in migration

--- oauth2_provider/migrations/0002_auto_20150522_1231.py
+++ oauth2_provider/migrations/0002_auto_20150522_1231.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
+from oauth2_provider.settings import oauth2_settings
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='refreshtoken',
             name='access_token',
-            field=models.OneToOneField(to=settings.OAUTH2_PROVIDER_ACCESS_TOKEN_MODEL, related_name='refresh_token'),
+            field=models.OneToOneField(to=oauth2_settings.ACCESS_TOKEN_MODEL, related_name='refresh_token'),
         ),
     ]
