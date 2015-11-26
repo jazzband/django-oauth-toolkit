d1e67990e34621bef2ef46cd80ffa3e58ff0dc54
Federico Frenguelli <synasius@gmail.com>
2015-11-26 14:31:14 +0100

    moved to pytest
    lower the number of cpus used by xdist
    
    fixed typo
    
    foo

--- runtests.py
+++ runtests.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-import os
-import sys
+import pytest
 
-if __name__ == "__main__":
-    app_to_test = "oauth2_provider"
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oauth2_provider.tests.settings")
-
-    from django.core.management import execute_from_command_line
-    execute_from_command_line([sys.argv[0], "test", app_to_test])
+pytest.main()
