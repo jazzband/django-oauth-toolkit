df5d5a1850159ef2fdbf825f30ea97effcd58dcf
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-12-15 16:30:38 +0100

    wrapping pytest.main() call in the runtests.py; fixing the script exit code

--- runtests.py
+++ runtests.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import sys
 import pytest
 
-pytest.main()
+
+# sys.exit() is required otherwise the wrapper exits
+# with exit code 0, regardless the pytest.main() execution
+sys.exit(pytest.main())
