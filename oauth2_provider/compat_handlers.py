0c67687046bbda4ff1b3a5eb825eeac148346260
Bart Merenda <outsbart@gmail.com>
2015-12-15 18:26:46 +0100

    removed in-line # noqa

--- oauth2_provider/compat_handlers.py
+++ oauth2_provider/compat_handlers.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 # Django 1.9 drops the NullHandler since Python 2.7 includes it
 try:
-    from logging import NullHandler  # noqa
+    from logging import NullHandler
 except ImportError:
-    from django.utils.log import NullHandler   # noqa
+    from django.utils.log import NullHandler
