144e7d0916d134200669a5ec1b293a8131c700f2
Omer Katz <omer.drow@gmail.com>
2016-02-12 00:31:06 +0200

    Avoid clash in related names.

--- oauth2_provider/tests/models.py
+++ oauth2_provider/tests/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from oauth2_provider.models import (AbstractApplication,
-                                    AbstractGrant,
-                                    AbstractAccessToken,
-                                    AbstractRefreshToken)
+from oauth2_provider.compat import AUTH_USER_MODEL
+from oauth2_provider.models import (AbstractAccessToken, AbstractApplication,
+                                    AbstractGrant)
+from oauth2_provider.settings import oauth2_settings
 
 
 class TestApplication(AbstractApplication):
@@ -14,5 +14,19 @@ class TestGrant(AbstractGrant):
 class TestAccessToken(AbstractAccessToken):
     custom_field = models.CharField(max_length=255)
 
-class TestRefreshToken(AbstractRefreshToken):
-    custom_field = models.CharField(max_length=255)
+class TestRefreshToken(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL)
+    token = models.CharField(max_length=255, db_index=True)
+    application = models.ForeignKey(oauth2_settings.APPLICATION_MODEL)
+    access_token = models.OneToOneField(oauth2_settings.ACCESS_TOKEN_MODEL,
+                                        related_name='test_refresh_token')
+
+    def revoke(self):
+        """
+        Delete this refresh token along with related access token
+        """
+        AccessToken.objects.get(id=self.access_token.id).revoke()
+        self.delete()
+
+    def __str__(self):
+        return self.token
