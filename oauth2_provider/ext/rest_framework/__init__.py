9c86cfac3426bc17e0aac41e3bbcb08f1f711e6d
Jens Timmerman <jens.timmerman@ugent.be>
2016-06-29 18:40:38 +0200

    import permission into rest_framework

--- oauth2_provider/ext/rest_framework/__init__.py
+++ oauth2_provider/ext/rest_framework/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from .authentication import OAuth2Authentication
 from .permissions import TokenHasScope, TokenHasReadWriteScope, TokenHasResourceScope
+from .permissions import IsAuthenticatedOrTokenHasScope
