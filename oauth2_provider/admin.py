0ff422a7e5e39f5b0a3844a289de242c16f34011
Jerome Leclanche <jerome@leclan.ch>
2016-12-10 12:13:49 +0200

    Greatly improve model administration

--- oauth2_provider/admin.py
+++ oauth2_provider/admin.py
@@ -3,13 +3,34 @@ from django.contrib import admin
 from .models import Grant, AccessToken, RefreshToken, get_application_model
 
 
-class RawIDAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "client_type", "authorization_grant_type")
+    list_filter = ("client_type", "authorization_grant_type", "skip_authorization")
+    radio_fields = {
+        "client_type": admin.HORIZONTAL,
+        "authorization_grant_type": admin.VERTICAL,
+    }
+    raw_id_fields = ("user", )
+
+
+class GrantAdmin(admin.ModelAdmin):
+    list_display = ("code", "application", "user", "expires")
+    raw_id_fields = ("user", )
+
+
+class AccessTokenAdmin(admin.ModelAdmin):
+    list_display = ("token", "user", "application", "expires")
+    raw_id_fields = ("user", )
+
+
+class RefreshTokenAdmin(admin.ModelAdmin):
+    list_display = ("token", "user", "application", "expires")
+    raw_id_fields = ("user", "access_token")
 
 
 Application = get_application_model()
 
-admin.site.register(Application, RawIDAdmin)
-admin.site.register(Grant, RawIDAdmin)
-admin.site.register(AccessToken, RawIDAdmin)
-admin.site.register(RefreshToken, RawIDAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Grant, GrantAdmin)
+admin.site.register(AccessToken, AccessTokenAdmin)
+admin.site.register(RefreshToken, RefreshTokenAdmin)
