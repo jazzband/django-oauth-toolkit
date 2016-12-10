0bcdbd770a31dbe360025b87858435b3180ab05a
Jerome Leclanche <jerome@leclan.ch>
2016-12-10 12:20:53 +0200

    Separate the base views and management views in url router
    This allows users to selectively include the base urlpatterns and the
    management urlpatterns, or include them behind separate root urls.

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -3,23 +3,26 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
+
+base_urlpatterns = [
     url(r'^authorize/$', views.AuthorizationView.as_view(), name="authorize"),
     url(r'^token/$', views.TokenView.as_view(), name="token"),
     url(r'^revoke_token/$', views.RevokeTokenView.as_view(), name="revoke-token"),
 ]
 
-# Application management views
-urlpatterns += [
+
+management_urlpatterns = [
+    # Application management views
     url(r'^applications/$', views.ApplicationList.as_view(), name="list"),
     url(r'^applications/register/$', views.ApplicationRegistration.as_view(), name="register"),
     url(r'^applications/(?P<pk>\d+)/$', views.ApplicationDetail.as_view(), name="detail"),
     url(r'^applications/(?P<pk>\d+)/delete/$', views.ApplicationDelete.as_view(), name="delete"),
     url(r'^applications/(?P<pk>\d+)/update/$', views.ApplicationUpdate.as_view(), name="update"),
-]
-
-urlpatterns += [
+    # Token management views
     url(r'^authorized_tokens/$', views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
     url(r'^authorized_tokens/(?P<pk>\d+)/delete/$', views.AuthorizedTokenDeleteView.as_view(),
         name="authorized-token-delete"),
 ]
+
+
+urlpatterns = base_urlpatterns + management_urlpatterns
