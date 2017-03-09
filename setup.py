3fdc7e66e47886137acd3c0322b0d8c02e56c797
Jerome Leclanche <jerome@leclan.ch>
2017-03-09 07:01:48 +0200

    Move most setup metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,58 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-import os
-import re
+from setuptools import setup
 
 
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
-
-
-version = get_version('oauth2_provider')
-
-
-LONG_DESCRIPTION = open('README.rst').read()
-
-setup(
-    name="django-oauth-toolkit",
-    version=version,
-    description="OAuth2 goodies for Django",
-    long_description=LONG_DESCRIPTION,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.9",
-        "Framework :: Django :: 1.10",
-    ],
-    keywords='django oauth oauth2 oauthlib',
-    author="Federico Frenguelli, Massimiliano Pippi",
-    author_email='synasius@gmail.com, mpippi@gmail.com',
-    url='https://github.com/evonove/django-oauth-toolkit',
-    license='BSD',
-    packages=find_packages(),
-    include_package_data=True,
-    test_suite='runtests',
-    install_requires=[
-        'django>=1.8',
-        'django-braces>=1.11.0',
-        'oauthlib==2.0.1',
-        'six',
-    ],
-    zip_safe=False,
-)
+setup(test_suite="runtests")
