e0bdf32af7deac928ae56da8362076d285bc7e4d
Tobias Gall <tobias.gall@mailbox.org>
2018-03-18 10:48:26 +0100

    Don't require write scope for introspection Replacing `ReadWriteScopedResourceView` with `ScopedProtectedResourceView`
    fixes #555

--- tests/test_introspection_auth.py
+++ tests/test_introspection_auth.py
@@ -96,7 +96,7 @@ class TestTokenIntrospectionAuth(TestCase):
             user=self.resource_server_user, token="12345678900",
             application=self.application,
             expires=timezone.now() + datetime.timedelta(days=1),
-            scope="read write introspection"
+            scope="introspection"
         )
 
         self.invalid_token = AccessToken.objects.create(
