e3aef5b58349023beb411d68f1360263189d0e15
AmirReza <ar.ghods@gmail.com>
2018-10-30 14:19:28 -0400

    - use "source_refresh_token" for getting the refresh_token of an access_token - add an assertion for checking if it's returning the same refresh_token for the grace period or not.

--- oauth2_provider/oauth2_validators.py
+++ oauth2_provider/oauth2_validators.py
@@ -530,8 +530,8 @@ class OAuth2Validator(RequestValidator):
                 else:
                     # make sure that the token data we're returning matches
                     # the existing token
-                    token["refresh_token"] = previous_access_token.refresh_token.token
                     token["access_token"] = previous_access_token.token
+                    token["refresh_token"] = previous_access_token.source_refresh_token.token
                     token["scope"] = previous_access_token.scope
 
         # No refresh token should be created, just access token
