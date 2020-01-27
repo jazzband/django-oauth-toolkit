82316ddd56607afa0df5602ef6008f03f42bef2d
Armands Vijups <armands@e-cilveks.lv>
2020-01-27 11:00:51 +0000

    clean

--- oauth2_provider/management/commands/cleartokens.py
+++ oauth2_provider/management/commands/cleartokens.py
@@ -5,12 +5,12 @@ from ...models import clear_expired
 
 
 class Command(BaseCommand):
+    """Clear expired tokens, add custom date to limit clear tokens before this date."""
     help = "Can be run as a cronjob or directly to clean out expired tokens"
     
     def add_arguments(self, parser):
         parser.add_argument('--before')
 
-        
     def handle(self, *args, **options):
         before = options.get('before')
         try:
