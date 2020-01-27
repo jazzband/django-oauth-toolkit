08265d58954036d44731aa749d8167d05eefa8a6
Armands Vijups <armands@e-cilveks.lv>
2020-01-27 10:40:32 +0000

    try

--- oauth2_provider/management/commands/cleartokens.py
+++ oauth2_provider/management/commands/cleartokens.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
     help = "Can be run as a cronjob or directly to clean out expired tokens"
     
     def add_arguments(self, parser):
-        parser.add_argument('before')
+        parser.add_argument('--before')
 
         
     def handle(self, *args, **options):
-        before = options.get('before')
+        before = options.get('--before')
         import pdb; pdb.set_trace()
         clear_expired(before)
