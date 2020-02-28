a016afe8ef53ef05d0f36ce440d79d37556544cb
Alan Crosswell <alan@columbia.edu>
2020-02-28 16:30:10 -0500

    make docs/changelog.rst include CHANGELOG.md

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ django.setup()
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'rfc',]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'rfc', 'm2r',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
