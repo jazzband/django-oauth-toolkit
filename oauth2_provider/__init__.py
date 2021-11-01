5874bc2016f14cb1f74d86b4d4def2b646376fd3
Vinay Karanam <vinayinvicible@gmail.com>
2021-11-01 12:09:31 +0530

    Replaced pkg_resources usage with importlib.metadata

--- oauth2_provider/__init__.py
+++ oauth2_provider/__init__.py
@@ -1,6 +1,9 @@
-import pkg_resources
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
 
-__version__ = pkg_resources.require("django-oauth-toolkit")[0].version
+__version__ = version("django-oauth-toolkit")
 
 default_app_config = "oauth2_provider.apps.DOTConfig"
