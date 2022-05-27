f1f8293f5cafdb7f3b17ab8f224ebe5820bb9a6e
Joseph Abrahams <joseph@abrahams.io>
2022-05-27 16:02:32 -0600

    Test swapped migrations

---
+++ tests/settings_swapped.py
@@ -0,0 +1,6 @@
+from .settings import *  # noqa
+
+
+OAUTH2_PROVIDER_ACCESS_TOKEN_MODEL = "tests.SampleAccessToken"
+OAUTH2_PROVIDER_APPLICATION_MODEL = "tests.SampleApplication"
+OAUTH2_PROVIDER_REFRESH_TOKEN_MODEL = "tests.SampleRefreshToken"
