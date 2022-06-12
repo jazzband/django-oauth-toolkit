3a8eb77a758aa4e317e6fe121541b5d696bce394
Jay Turner <jay@trainerdex.app>
2022-06-12 19:25:26 +0100

    Add required_scopes to the __init__ file

--- oauth2_provider/contrib/rest_framework/__init__.py
+++ oauth2_provider/contrib/rest_framework/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 from .authentication import OAuth2Authentication
+from .decorators import required_scopes
 from .permissions import (
     IsAuthenticatedOrTokenHasScope,
     TokenHasReadWriteScope,
