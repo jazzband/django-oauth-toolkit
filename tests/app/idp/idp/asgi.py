50dff93b0520cd02d3bafab5ab4a005e3a55eae6
Darrel O'Pry <darrel.opry@spry-group.com>
2023-05-01 16:43:11 -0400

    feat: idp/rp test apps
    Implement a minimal testing IDP and RP for maintainers.
    
    There is a single Application configured in the IDP for
    the RP sample application it used the OIDC Authorization
    + PKCE flow.
    
    This is a meant to be a starting point for building out
    further test scenarios.

---
+++ tests/app/idp/idp/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for idp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idp.settings")
+
+application = get_asgi_application()
