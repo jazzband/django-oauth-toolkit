af9ce737bd557be43a909c2a09c6ca12924910d4
Yura Savin <yurasavin@gmail.com>
2023-06-30 06:51:11 +0000

    Issue 1295. Added revert action for migration

--- oauth2_provider/migrations/0006_alter_application_client_secret.py
+++ oauth2_provider/migrations/0006_alter_application_client_secret.py
@@ -26,5 +26,5 @@ class Migration(migrations.Migration):
             name='client_secret',
             field=oauth2_provider.models.ClientSecretField(blank=True, db_index=True, default=oauth2_provider.generators.generate_client_secret, help_text='Hashed on Save. Copy it now if this is a new secret.', max_length=255),
         ),
-        migrations.RunPython(forwards_func),
+        migrations.RunPython(forwards_func, migrations.RunPython.noop),
     ]
