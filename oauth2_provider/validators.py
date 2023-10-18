94213eafd5170af29c31f6461d7968674ad1de7e
Aliaksei Kanstantsinau <alekseikonstantinov@gmail.com>
2023-10-18 12:50:26 +0300

    Added ALLOWED_SCHEMES setting for Allowed Orgins validation

--- oauth2_provider/validators.py
+++ oauth2_provider/validators.py
@@ -31,6 +31,32 @@ class RedirectURIValidator(URIValidator):
             raise ValidationError("Redirect URIs must not contain fragments")
 
 
+class AllowedURIValidator(URIValidator):
+    def __init__(self, schemes, name, allow_path=False, allow_query=False, allow_fragments=False):
+        """
+        :params schemes: List of allowed schemes. E.g.: ["https"]
+        :params name: Name of the validater URI required for validation message. E.g.: "Origin"
+        :params allow_path: If URI can contain path part
+        :params allow_query: If URI can contain query part
+        :params allow_fragments: If URI can contain fragments part
+        """
+        super().__init__(schemes=schemes)
+        self.name = name
+        self.allow_path = allow_path
+        self.allow_query = allow_query
+        self.allow_fragments = allow_fragments
+
+    def __call__(self, value):
+        super().__call__(value)
+        value = force_str(value)
+        scheme, netloc, path, query, fragment = urlsplit(value)
+        if path and not self.allow_path:
+            raise ValidationError("{} URIs must not contain path".format(self.name))
+        if query and not self.allow_query:
+            raise ValidationError("{} URIs must not contain query".format(self.name))
+        if fragment and not self.allow_fragments:
+            raise ValidationError("{} URIs must not contain fragments".format(self.name))
+
 ##
 # WildcardSet is a special set that contains everything.
 # This is required in order to move validation of the scheme from
