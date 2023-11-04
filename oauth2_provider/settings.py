3dc240ac907055d3c3858c5ed073030b33536d38
Alan Rominger <arominge@redhat.com>
2023-11-03 20:59:47 -0400

    Move signal import to django.core

--- oauth2_provider/settings.py
+++ oauth2_provider/settings.py
@@ -19,7 +19,7 @@ back to the defaults.
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest
-from django.test.signals import setting_changed
+from django.core.signals import setting_changed
 from django.urls import reverse
 from django.utils.module_loading import import_string
 from oauthlib.common import Request
