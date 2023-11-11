a30001ff2a2e90bfd2b31152f0c513deeb1e2cc1
Darrel O'Pry <dopry@users.noreply.github.com>
2023-11-11 13:55:08 -0500

    Fix/test app rp openid configuration (#1362)
    * fix: cors on .well-know redirect in test app
    
    (cherry picked from commit a592988d1c61635c7ef6b568b0f1c51a3912a06f)
    
    * fix: mismatched issuer origin for idp

--- tests/app/idp/idp/apps.py
+++ tests/app/idp/idp/apps.py
@@ -3,7 +3,12 @@ from django.apps import AppConfig
 
 
 def cors_allow_origin(sender, request, **kwargs):
-    return request.path == "/o/userinfo/" or request.path == "/o/userinfo"
+    return (
+        request.path == "/o/userinfo/"
+        or request.path == "/o/userinfo"
+        or request.path == "/o/.well-known/openid-configuration"
+        or request.path == "/o/.well-known/openid-configuration/"
+    )
 
 
 class IDPAppConfig(AppConfig):
