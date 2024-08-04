782dc4473b3ac63ee5088806ca86a505dea247ab
blag <blag@users.noreply.github.com>
2024-08-04 04:46:22 -0700

    Remove hash_client_secret from the application update view

--- oauth2_provider/views/application.py
+++ oauth2_provider/views/application.py
@@ -94,7 +94,6 @@ class ApplicationUpdate(ApplicationOwnerIsUserMixin, UpdateView):
             fields=(
                 "name",
                 "client_id",
-                "hash_client_secret",
                 "client_type",
                 "authorization_grant_type",
                 "redirect_uris",
