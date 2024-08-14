db5a4784d2ca3b9659ad210f8d1c5fcf2fd17af1
Dulmandakh <dulmandakh@gmail.com>
2024-08-14 17:17:29 +0800

    use path in urls

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path, re_path
 
 from . import views
 
@@ -7,24 +7,23 @@ app_name = "oauth2_provider"
 
 
 base_urlpatterns = [
-    re_path(r"^authorize/$", views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^token/$", views.TokenView.as_view(), name="token"),
-    re_path(r"^revoke_token/$", views.RevokeTokenView.as_view(), name="revoke-token"),
-    re_path(r"^introspect/$", views.IntrospectTokenView.as_view(), name="introspect"),
+    path("authorize/", views.AuthorizationView.as_view(), name="authorize"),
+    path("token/", views.TokenView.as_view(), name="token"),
+    path("revoke_token/", views.RevokeTokenView.as_view(), name="revoke-token"),
+    path("introspect/", views.IntrospectTokenView.as_view(), name="introspect"),
 ]
 
 
 management_urlpatterns = [
     # Application management views
-    re_path(r"^applications/$", views.ApplicationList.as_view(), name="list"),
-    re_path(r"^applications/register/$", views.ApplicationRegistration.as_view(), name="register"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/$", views.ApplicationDetail.as_view(), name="detail"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/delete/$", views.ApplicationDelete.as_view(), name="delete"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/update/$", views.ApplicationUpdate.as_view(), name="update"),
+    path("applications/", views.ApplicationList.as_view(), name="list"),
+    path("applications/register/", views.ApplicationRegistration.as_view(), name="register"),
+    path("applications/(?P<pk>[\w-]+)/", views.ApplicationDetail.as_view(), name="detail"),
+    path("applications/(?P<pk>[\w-]+)/delete/", views.ApplicationDelete.as_view(), name="delete"),
+    path("applications/(?P<pk>[\w-]+)/update/", views.ApplicationUpdate.as_view(), name="update"),
     # Token management views
-    re_path(r"^authorized_tokens/$", views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-    re_path(
-        r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
+    path("authorized_tokens/", views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+    path("authorized_tokens/(?P<pk>[\w-]+)/delete/",
         views.AuthorizedTokenDeleteView.as_view(),
         name="authorized-token-delete",
     ),
@@ -40,9 +39,9 @@ oidc_urlpatterns = [
         views.ConnectDiscoveryInfoView.as_view(),
         name="oidc-connect-discovery-info",
     ),
-    re_path(r"^\.well-known/jwks.json$", views.JwksInfoView.as_view(), name="jwks-info"),
-    re_path(r"^userinfo/$", views.UserInfoView.as_view(), name="user-info"),
-    re_path(r"^logout/$", views.RPInitiatedLogoutView.as_view(), name="rp-initiated-logout"),
+    path("\.well-known/jwks.json", views.JwksInfoView.as_view(), name="jwks-info"),
+    path("userinfo/", views.UserInfoView.as_view(), name="user-info"),
+    path("logout/", views.RPInitiatedLogoutView.as_view(), name="rp-initiated-logout"),
 ]
 
 
