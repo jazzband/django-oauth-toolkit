9182370155982cd0e7bc5a85c958a83edc00e10e
Dulmandakh <dulmandakh@gmail.com>
2024-08-14 17:23:40 +0800

    fix args

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -18,13 +18,12 @@ management_urlpatterns = [
     # Application management views
     path("applications/", views.ApplicationList.as_view(), name="list"),
     path("applications/register/", views.ApplicationRegistration.as_view(), name="register"),
-    path("applications/(?P<pk>[\w-]+)/", views.ApplicationDetail.as_view(), name="detail"),
-    path("applications/(?P<pk>[\w-]+)/delete/", views.ApplicationDelete.as_view(), name="delete"),
-    path("applications/(?P<pk>[\w-]+)/update/", views.ApplicationUpdate.as_view(), name="update"),
+    path("applications/<slug:pk>/", views.ApplicationDetail.as_view(), name="detail"),
+    path("applications/<slug:pk>/delete/", views.ApplicationDelete.as_view(), name="delete"),
+    path("applications/<slug:pk>/update/", views.ApplicationUpdate.as_view(), name="update"),
     # Token management views
     path("authorized_tokens/", views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-    path(
-        "authorized_tokens/(?P<pk>[\w-]+)/delete/",
+    path("authorized_tokens/<slug:pk>/delete/",
         views.AuthorizedTokenDeleteView.as_view(),
         name="authorized-token-delete",
     ),
