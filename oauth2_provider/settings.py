1c52caf3518277fce5d01e085b6597b6b25eb6d6
David Uzumaki <56260075+duzumaki@users.noreply.github.com>
2025-01-06 12:41:44 +0000

    Add device settings
    OAUTH_DEVICE_VERIFICATION_URI = the uri that comes back from the response
    so the user knows where to go to. e.g example.com/device
    
    OAUTH_DEVICE_USER_CODE_GENERATOR = Allows a custom callable to be passed in to control
    how the user code is generated, stored in the db and returned back to the caller
    DEVICE_MODEL = the device model
    
    DEVICE_FLOW_INTERVAL = The time in seconds to wait before the device should poll again

--- oauth2_provider/settings.py
+++ oauth2_provider/settings.py
@@ -28,6 +28,7 @@ from oauthlib.common import Request
 USER_SETTINGS = getattr(settings, "OAUTH2_PROVIDER", None)
 
 APPLICATION_MODEL = getattr(settings, "OAUTH2_PROVIDER_APPLICATION_MODEL", "oauth2_provider.Application")
+DEVICE_MODEL = getattr(settings, "OAUTH2_PROVIDER_DEVICE_MODEL", "oauth2_provider.Device")
 ACCESS_TOKEN_MODEL = getattr(settings, "OAUTH2_PROVIDER_ACCESS_TOKEN_MODEL", "oauth2_provider.AccessToken")
 ID_TOKEN_MODEL = getattr(settings, "OAUTH2_PROVIDER_ID_TOKEN_MODEL", "oauth2_provider.IDToken")
 GRANT_MODEL = getattr(settings, "OAUTH2_PROVIDER_GRANT_MODEL", "oauth2_provider.Grant")
@@ -39,6 +40,8 @@ DEFAULTS = {
     "CLIENT_SECRET_GENERATOR_LENGTH": 128,
     "CLIENT_SECRET_HASHER": "default",
     "ACCESS_TOKEN_GENERATOR": None,
+    "OAUTH_DEVICE_VERIFICATION_URI": None,
+    "OAUTH_DEVICE_USER_CODE_GENERATOR": None,
     "REFRESH_TOKEN_GENERATOR": None,
     "EXTRA_SERVER_KWARGS": {},
     "OAUTH2_SERVER_CLASS": "oauthlib.oauth2.Server",
@@ -61,6 +64,8 @@ DEFAULTS = {
     "APPLICATION_MODEL": APPLICATION_MODEL,
     "ACCESS_TOKEN_MODEL": ACCESS_TOKEN_MODEL,
     "ID_TOKEN_MODEL": ID_TOKEN_MODEL,
+    "DEVICE_MODEL": DEVICE_MODEL,
+    "DEVICE_FLOW_INTERVAL": 5,
     "GRANT_MODEL": GRANT_MODEL,
     "REFRESH_TOKEN_MODEL": REFRESH_TOKEN_MODEL,
     "APPLICATION_ADMIN_CLASS": "oauth2_provider.admin.ApplicationAdmin",
@@ -268,6 +273,9 @@ class OAuth2ProviderSettings:
                 ("refresh_token_expires_in", "REFRESH_TOKEN_EXPIRE_SECONDS"),
                 ("token_generator", "ACCESS_TOKEN_GENERATOR"),
                 ("refresh_token_generator", "REFRESH_TOKEN_GENERATOR"),
+                ("verification_uri", "OAUTH_DEVICE_VERIFICATION_URI"),
+                ("interval", "DEVICE_FLOW_INTERVAL"),
+                ("user_code_generator", "OAUTH_DEVICE_USER_CODE_GENERATOR"),
             ]
         }
         kwargs.update(self.EXTRA_SERVER_KWARGS)
